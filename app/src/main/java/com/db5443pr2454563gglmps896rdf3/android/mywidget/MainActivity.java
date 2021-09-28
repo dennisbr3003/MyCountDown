ead1fbfb52de776024684dc957375e3309f9f78e
Dennis Brink <dennis.mjknd@gmail.com>
2021-09-28 23:57:55 +0200

    Added recyclerview (with dummy data) in activity main

--- app/src/main/java/com/db5443pr2454563gglmps896rdf3/android/mywidget/MainActivity.java
+++ app/src/main/java/com/db5443pr2454563gglmps896rdf3/android/mywidget/MainActivity.java
@@ -1,14 +1,43 @@
 package com.db5443pr2454563gglmps896rdf3.android.mywidget;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements WidgetViewAdapter.ItemClickListener{
+
+    WidgetViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // data to populate the RecyclerView with
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.WidgetView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new WidgetViewAdapter(this, animalNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+
     }
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
+
 }
